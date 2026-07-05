625a0836e8986c6a6f41ec05984ed015832f4f8b
Arjun gupta <131136198+Arjung352@users.noreply.github.com>
2026-07-05 23:19:47 +0530

    solved Find middle problem

---
+++ ArrayProblems/Find middle.java
@@ -0,0 +1,20 @@
+class FindMiddle {
+    public boolean isMiddleElementUnique(int[] nums) {
+        //here what we can do is retrive the mid value and use two pointers and compare consiqutively with middle element
+        if(nums.length==1){
+            return true;
+        }
+        int mid=nums[nums.length/2];
+        int left=0;
+        int right=nums.length-1;
+        // while loop comparing 2 elements each time
+        while(left<right){
+            if(nums[left]==mid||nums[right]==mid){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
